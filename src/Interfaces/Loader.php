f9688b60e20103a54ecf88f9e6bf71cb82c6ae18
hone <hone@hone.(none)>
2014-07-02 13:21:54 +1000

    adjust license date

--- src/Interfaces/Loader.php
+++ src/Interfaces/Loader.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @author Hone Watson
- * @email hone.watson@citrus.com.au
- * @copyright  Copyright (c) 2013 Hone Watson
+ * @email code@hone.be
+ * @copyright  Copyright (c) 2014 Hone Watson
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
--- src/Loader.php
+++ src/Loader.php
@@ -2,7 +2,7 @@
 /**
  * @author Hone Watson
  * @email code@hone.be
- * @copyright  Copyright (c) 2013 Hone Watson
+ * @copyright  Copyright (c) 2014 Hone Watson
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
